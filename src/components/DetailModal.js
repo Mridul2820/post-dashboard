0734c0598e9cfd0be69b82f8a0c51ef2a743a2b7
Mridul2820 <immridulpanda2820@gmail.com>
2021-06-11 16:37:53 +0530

    modal content done

--- src/components/DetailModal.js
+++ src/components/DetailModal.js
@@ -14,6 +14,15 @@ const DetailModal = ({ modalData, setModalOpen }) => {
         <Container className="backdrop" onClick={handleClick} >
             <ModalMain>
                 <h1>{modalData.title}</h1>
+                <Img>
+                    <img 
+                        src={modalData.image
+                            ? modalData.image
+                            : 'https://source.unsplash.com/random/300x200'
+                        }
+                        alt="post" 
+                    />
+                </Img>
                 <h2>{modalData.summary}</h2>
                 <p>{modalData.published}</p>
             </ModalMain>
@@ -38,6 +47,39 @@ const ModalMain = styled.div`
     width: 700px;
     height: 400px;
     border-radius: 10px;
+    padding: 15px 25px;
+    overflow-y: scroll;
+
+    &::-webkit-scrollbar {
+        width: 16px;
+    }
+    &::-webkit-scrollbar-thumb {
+        background: #dadce0;
+        background-clip: padding-box;
+        border: 4px solid transparent;
+        border-radius: 8px;
+        box-shadow: none;
+        min-height: 50px;
+    }
+
+    h1 {
+        font-size: 22px;
+    }
+
+    h2 {
+        font-size: 18px;
+        margin-bottom: 15px;
+    }
+`
+
+const Img  = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        margin: 20px auto;
+    }
 `
 
 
